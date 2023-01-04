4622f0e541e000becb3b5e72ca49a4c3a01bd3c1
Aman <amankapil60@gmail.com>
2023-01-04 16:50:14 +0530

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Attendence from "./Components/Attendence";
+import StudentAttendance from "./Components/Gpt";
+
+function App() {
+  return (
+    <div className="App">
+      <Attendence />
+      {/* <StudentAttendance /> */}
+    </div>
+  );
+}
+
+export default App;
