236c3676999aac372fa2d33f7782f6fc9445202e
Aman <amankapil60@gmail.com>
2023-07-16 00:10:16 +0530

    react_done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Attendence from "./Components/Attendence";
 import StudentAttendance from "./Components/Gpt";
+import Rajhome from "./Components/Rajhome";
 
 function App() {
   return (
     <div className="App">
-      <Attendence />
+      {/* <Attendence /> */}
+      <Rajhome />
       {/* <StudentAttendance /> */}
     </div>
   );
